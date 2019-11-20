5dd2720e302cff97ebb34d08d0f4f2fd753a6225
chrisegan623 <54693774+chrisegan623@users.noreply.github.com>
2019-11-20 14:52:49 -0500

    Update code.py

--- code.py
+++ code.py
@@ -3,28 +3,25 @@ from sklearn.datasets import load_boston
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 
-if __name__ == '__main__':
-    boston = load_boston()
-    data = pd.DataFrame(boston.data)            #convert to pandas dataframe
-    data.columns = boston.feature_names         #set column names to feature names
-    data['Price'] = boston.target               #add price column
+def sklearn(x):
+    data = pd.DataFrame(x.data)  # convert to pandas dataframe
+    data.columns = x.feature_names  # set column names to feature names
+    data['Price'] = x.target  # add price column
 
-
-    X = data.drop(['Price'], axis = 1)          #set X and Y data for linear regression
+    X = data.drop(['Price'], axis=1)  # set X and Y data for linear regression
     Y = data['Price']
 
-    model = LinearRegression().fit(X,Y)
+    model = LinearRegression().fit(X, Y)
     influence = pd.DataFrame(model.coef_)
     influence.columns = ['Coef']
-    influence['Feature'] = boston.feature_names
-    #This dataframe shows each factors influence
-
+    influence['Feature'] = x.feature_names
+    # This dataframe shows each factors influence
 
     find_max = pd.DataFrame(abs(model.coef_))
     # create dataframe with all positive values to find the maximum
 
     find_max.columns = ['Coef']
-    find_max['Feature'] = boston.feature_names
+    find_max['Feature'] = x.feature_names
 
     find_max['Influence'] = 'Positive'
 
@@ -35,4 +32,7 @@ if __name__ == '__main__':
     MaxInfluence = (find_max[find_max.Coef == find_max.Coef.max()])
 
     with open('Boston.txt', 'w') as b:
-            b.write(str(MaxInfluence))
+        b.write(str(MaxInfluence))
+
+if __name__ == '__main__':
+    sklearn(load_boston())
